4bf96ce079e4ede04f0a5dbc535f7ad6d3ba5665
seenmyfate <seenmyfate@gmail.com>
2011-10-13 09:00:48 +0100

    added specs and code

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -1,3 +1,7 @@
 class Basket < ActiveRecord::Base
   has_many :basket_items
+
+  def total_discount
+    basket_items.collect(&:discount).sum
+  end
 end
--- spec/models/basket_spec.rb
+++ spec/models/basket_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
-
 describe Basket do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "total_discount" do
+    let(:basket) { Basket.create! }
+    let(:basket_items) { [BasketItem.create!(:discount => 10),
+      BasketItem.create!(:discount => 20) ]}
+
+    it "should return the total discount" do
+      basket = Basket.create!
+      basket.basket_items = basket_items
+      basket.total_discount.should == 30
+    end
+  end
 end
