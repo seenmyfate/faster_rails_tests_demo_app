d6f8d234e2d1af4fb519d3a2b1e1e21d5de0ba66
seenmyfate <seenmyfate@gmail.com>
2011-10-13 14:21:43 +0100

    1002 examples in 1 second

--- spec/lib/discount_calculator_spec.rb
+++ spec/lib/discount_calculator_spec.rb
@@ -1,12 +1,15 @@
 require 'active_support/core_ext/enumerable'
 require 'discount_calculator'
 describe DiscountCalculator do
-  context "#total_discount" do
-    let(:items) { [stub(:discount => 10),
-      stub(:discount => 20)] }
-    it "should return the total discount" do
-      calculator = DiscountCalculator.new
-      calculator.total_discount(items).should eq 30
+  
+  1000.times do
+    context "#total_discount" do
+      let(:items) { [stub(:discount => 10),
+        stub(:discount => 20)] }
+      it "should return the total discount" do
+        calculator = DiscountCalculator.new
+        calculator.total_discount(items).should eq 30
+      end
     end
   end
 end
